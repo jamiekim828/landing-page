94365f24db403262379e961c065ca6012f98661b
jamiekim828 <jamiekim828@gmail.com>
2020-02-11 01:03:27 +0100

    hide/appear menu on scroll

--- js/app.js
+++ js/app.js
@@ -171,14 +171,31 @@ window.onclick = function(event) {
   }
 };
 
-// hide menu
-let oldScroll = 0;
-let currentScroll = 0;
-setTimeout(function() {
-  if (currentScroll === oldScroll) {
-    document.querySelector('.page__header').style.visibility = 'hidden';
-  } else {
-    document.querySelector('.page__header').style.visibility = 'visible';
+// hide and appear menu when scroll
+
+const body = document.body;
+const scrollUp = 'scroll-up';
+const scrollDown = 'scroll-down';
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll == 0) {
+    body.classList.remove(scrollUp);
+    return;
   }
-  oldScroll = currentScroll;
-}, 10000);
+
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains(scrollDown)
+  ) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
